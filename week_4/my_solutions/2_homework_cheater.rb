5768f6cad01be89362df9ec7d281088bdc5c92b2
Greg <greg.knudsen09@gmail.com>
2014-04-03 14:52:28 -0400

    Update challenge 2 after pairing
    Driver code works now.

--- week_4/my_solutions/2_homework_cheater.rb
+++ week_4/my_solutions/2_homework_cheater.rb
@@ -1,13 +1,14 @@
 # U2.W4: Homework Cheater
 
 
-# I worked on this challenge by myself.
+# I worked on this challenge by myself - and then with Natalie a few days later
 
 # 2. Pseudocode
 
 # Input: a title, a topic, a date, a thesis statement, and a pronoun
 # Output: Using all of the parameters given, include them mixed with some stock text already input to the method.
 # Steps:
+
 =begin
 
 create method called essay writer with the 5 inputs above as parameters. 
@@ -15,7 +16,6 @@ create generic template of text to add to thesis statement.
 	store that text in variable for easier access
 write code that inputs the given paremeters using proper formatting syntax (new lines, line breaks, etc...)
 	and, along with the generic templates, create the essay.
-
 
 =end
 
@@ -42,8 +42,7 @@ def essay_writer(title, topic, date, thesis_statement, pronoun)
 	essay = "My Brilliant Essay on: #{title}\n\n#{topic} was a well known person in #{date}. I like reading about #{pronoun_use}. #{thesis_statement} I defnintely want to learn more!"
 end
 
-puts essay_writer("The Rite of Spring", "Igor Strivinsky", 1913, "The premiere of Strivinsky's Rite of Spring was a landmark moment in music history.\nThe music was so controversial that it casued a riot!", "male")
-puts essay_writer("The First 'First Lady'", "Martha Washington", 1789, "Even though the term 'First Lady' wasn't coined until after her death,\nMartha Washington played an important role in American history.", "female")
+
 
 
 
@@ -51,9 +50,11 @@ puts essay_writer("The First 'First Lady'", "Martha Washington", 1789, "Even tho
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-essay_writer(title) == "The Rite of Spring" # => doesn't work, along with all the other possibilities
-essay_writer.title == "The Rite of Spring" # => also doens't work...not sure why
+# essay_writer(title) == "The Rite of Spring" # => doesn't work, along with all the other possibilities
+# essay_writer.title == "The Rite of Spring" # => also doens't work...not sure why
 
+p essay_writer("The Rite of Spring", "Igor Stravinsky", 1913, "The premiere of Stravinsky's Rite of Spring was a landmark moment in music history. The music was so controversial that it casued a riot!", "male") == "My Brilliant Essay on: The Rite of Spring\n\nIgor Stravinsky was a well known person in 1913. I like reading about him. The premiere of Stravinsky's Rite of Spring was a landmark moment in music history. The music was so controversial that it casued a riot! I defnintely want to learn more!"
+p essay_writer("The First 'First Lady'", "Martha Washington", 1789, "Even though the term 'First Lady' wasn't coined until after her death, Martha Washington played an important role in American history.", "female") == "My Brilliant Essay on: The First 'First Lady'\n\nMartha Washington was a well known person in 1789. I like reading about her. Even though the term 'First Lady' wasn't coined until after her death, Martha Washington played an important role in American history. I defnintely want to learn more!"
 
 
 
@@ -66,6 +67,11 @@ other than the obvious 'is this a method' type things. I wanted to make sure the
 I added a simple ternary function to change the pronoun with the given input. I was, and still am, confused by the fact that the given example had the pronoun as a variable, not a string.
 I tried a few different ways to turn that parameter into a variable, but when I'd call it, it would say it's an undefined variable. I may revisit this again this week if I have time, as I'm not terribly
 happy with it, but I need to move on to the solo and group challenges.
+
+***UPDATE after pairing
+	After I paired with Natalie for this challenge, (I think) the driver code concept became much more clear. Basically, how I would explain it is to think about what you'll be doing at the very end of the 
+	process and think about what you want the output to be. Both driver code tests I wrote above were done after the fact, but it is something I feel much more comfortable with now. I look forward
+	to working with driver code and TDD more!
 
 
 =end
